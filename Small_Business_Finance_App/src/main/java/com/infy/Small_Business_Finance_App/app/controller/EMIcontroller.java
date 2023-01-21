b652c3541d6ec2e419198ae5c3695c55887b84a5
SURUCHI1212 <surunautiyal@gmail.com>
2023-01-21 20:02:51 +0530

    removed extra controllers and services.

---
+++ Small_Business_Finance_App/src/main/java/com/infy/Small_Business_Finance_App/app/controller/EMIcontroller.java
@@ -0,0 +1,55 @@
+package com.infy.Small_Business_Finance_App.app.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.infy.Small_Business_Finance_App.app.model.EMI;
+import com.infy.Small_Business_Finance_App.app.serviceI.EMIserviceI;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping(value = "/emiApi")
+public class EMIcontroller {
+
+	@Autowired
+	EMIserviceI emiS;
+	
+	@PostMapping(value = "/saveEmi")
+	public ResponseEntity<EMI> saveEMI(@RequestBody EMI emi)
+	{
+		EMI e=emiS.saveEMI(emi);
+		return new ResponseEntity<EMI>(e,HttpStatus.CREATED);
+	}
+	
+	@PutMapping(value = "/updateEmi/{id}")
+	public ResponseEntity<EMI> updateEMI(@RequestBody EMI emi,@PathVariable int id)
+	{
+		EMI e=emiS.updateEMI(emi,id);
+		return new ResponseEntity<EMI>(e,HttpStatus.OK);
+	}
+	
+	@GetMapping(value = "/getAllemi")
+	public ResponseEntity<List<EMI>> getAllemi()
+	{
+		List<EMI> emiL=emiS.getAll();
+		return new ResponseEntity<List<EMI>>(emiL,HttpStatus.OK);
+	}
+	
+	@GetMapping(value = "/getAllemi/{status}")
+	public ResponseEntity<List<EMI>> getEmiByStatus(@PathVariable String status)
+	{
+		List<EMI> emiL=emiS.getEmiByStatus(status);
+		return new ResponseEntity<List<EMI>>(emiL,HttpStatus.OK);
+	}
+}
--- Small_Business_Finance_App/src/main/java/com/infy/Small_Business_Finance_App/app/model/SanctionedLoanDetails.java
+++ Small_Business_Finance_App/src/main/java/com/infy/Small_Business_Finance_App/app/model/SanctionedLoanDetails.java
@@ -1,11 +1,9 @@
 package com.infy.Small_Business_Finance_App.app.model;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToOne;
 import lombok.Data;
 
 
@@ -19,10 +17,8 @@ public class SanctionedLoanDetails
 	private Integer sanctionedLoanId;
 	private String sanctionedLoanAmount;
 	private String sanctionedLoanTenure;
-	private String customerName;
-	private Integer customerId;
+	private Float rateOfInterest;
 	private Double monthlyEmi;
-	@OneToOne(cascade = CascadeType.ALL)
-	private EMI emi;
+	
 
 }
